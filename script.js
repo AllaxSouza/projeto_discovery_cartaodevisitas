822cda9e557dbeef678cbed9cb08444900812084
AllaxSouza <allaxsouza@hotmail.com>
2023-08-09 14:53:21 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleModel(){
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  const img = document.querySelector('#profile img')
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src","./assets/rodrigo.jpeg")
+  }
+  else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+}
