d0177016d965b1688c1d50dc5c58b9352979b2b4
AllaxSouza <allaxsouza@hotmail.com>
2023-08-09 15:04:08 -0300

    comentando a  função para troca de imagem

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function toggleModel(){
   const html = document.documentElement
   html.classList.toggle('light')
 
-  const img = document.querySelector('#profile img')
+  // const img = document.querySelector('#profile img')
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src","./assets/rodrigo.jpeg")
-  }
-  else {
-    img.setAttribute("src", "./assets/avatar.png")
-  }
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src","./assets/rodrigo.jpeg")
+  // }
+  // else {
+  //   img.setAttribute("src", "./assets/avatar.png")
+  // }
 
 }
